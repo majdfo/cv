6271bad05405780224489139a7d3eca54dfb504c
majdfo <126958150+majdfo@users.noreply.github.com>
2025-12-29 18:23:07 +0300

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,8 @@ import torchvision.transforms as T
 from io import BytesIO
 
 # Load the model
-model = torch.load('best.pt')
+model = torch.load('best.pt', weights_only=False)
+
 model.eval()  # Set the model to evaluation mode
 
 # Define the transformation (assuming your model uses common transforms like Resize, ToTensor)
